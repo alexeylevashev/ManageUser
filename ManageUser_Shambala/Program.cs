cbba8cb01c25dbf956e1391346f7bdb989199ce7
AnSt <starininandrey@gmail.com>
2021-03-21 12:28:48 +0300

    инициализация решения

---
+++ ManageUser_Shambala/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManageUser_Shambala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
